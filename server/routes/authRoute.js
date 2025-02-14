e87403b10df6b111b9ce6ff19c508c8615fb0ca2
devdesignerprakash <devdesignerprakash@gmail.com>
2025-02-14 07:02:14 +0545

    file name change: authvalidator to datavalidator

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,7 +3,7 @@ const router= express.Router()
 const authContollers= require('../controllers/authControllers')
 const signUpSchema= require('../validators/registrationValidator')
 const loginSchema=require('../validators/loginValidator')
-const validate = require('../middlewares/authValidator')
+const validate = require('../middlewares/dataValidator')
 router.route('/register').post(validate(signUpSchema),authContollers.regiseterUser)
 router.route('/login').post(validate(loginSchema),authContollers.login)
 
--- server/routes/contactRoute.js
+++ server/routes/contactRoute.js
@@ -2,6 +2,6 @@ const express= require('express')
 const router= express.Router()
 const contactControllers= require('../controllers/contactController')
 const contactSchema= require('../validators/contactValidator')
-const validate = require('../middlewares/authValidator')
+const validate = require('../middlewares/dataValidator')
 router.route('/message').post(validate(contactSchema),contactControllers.createMessage)
 module.exports=router
