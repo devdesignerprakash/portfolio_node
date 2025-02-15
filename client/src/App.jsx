f79d4dd40bb6f90b1c6bd7fdb8319ed5e2e98ba7
devdesignerprakash <devdesignerprakash@gmail.com>
2025-02-15 18:53:35 +0545

    admin panel and project schema is going to on porgress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,16 +10,10 @@ import Footer from './components/Footer'
 import ErrorPage from './pages/ErrorPage'
 import Logout from './pages/Logout'
 import Projects from './pages/Projects'
-import Dashboard from './admin/pages/Dashboard'
-import Sidebar from './admin/components/SideBar'
-import { useAuth } from './store/authStore'
-import AdminLayout from './admin/pages/AdminLayout'
 function App() {
-  const {user}= useAuth()
-  console.log(user)
   return (
     <BrowserRouter>
-      {user?.isAdmin?<Sidebar/>:<NavBar/>}
+      <NavBar/>
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
@@ -30,17 +24,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<ErrorPage />} />
-
-        {/* Admin Routes (Only for Admins) */}
-        {user?.isAdmin && (
-          <Route path="/" element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-            {/* <Route path="users" element={<ManageUsers />} />
-            <Route path="projects" element={<ManageProjects />} /> */}
-          </Route>
-        )}
-      </Routes>
-      <Footer/>
+        </Routes>
+    <Footer/>
     </BrowserRouter>
 
   )
