1e43547b766c2eefb364920b75f8ab7fe2d83228
devdesignerprakash <devdesignerprakash@gmail.com>
2025-02-28 13:35:55 +0545

    different profile links are aaded

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,19 +7,16 @@ const Footer = () => {
       <Container>
         <Row className="text-center">
           <Col md={12} className="mb-2">
-            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
+            <a href="https://www.facebook.com/prakash.neupane.52438" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
               <FaFacebook size={24} />
             </a>
-            <a href="mailto:yourmail@gmail.com" className="text-black mx-2">
-              <FaEnvelope size={24} />
-            </a>
-            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
+            <a href="https://instagram.comhttps://www.instagram.com/pra_kash_neupane/" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
               <FaInstagram size={24} />
             </a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
+            <a href="https://www.linkedin.com/in/prakash-neupane-391248278/" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
               <FaLinkedin size={24} />
             </a>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
+            <a href="https://github.com/devdesignerprakash" target="_blank" rel="noopener noreferrer" className="text-black mx-2">
               <FaGithub size={24} />
             </a>
           </Col>
